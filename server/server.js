5236903bf1a4901c83df2593275be8727a686c35
Tyler Bantley <tyler.bantley@icloud.com>
2022-03-02 15:14:32 -0600

    updated push

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const path = require('path');
 // const db = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
 
 // const server = new ApolloServer({
 //   typeDefs,
