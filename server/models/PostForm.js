b554571f9ad420ed221feac606e572f61c425f89
Grant Royseth <groyseth@gmail.com>
2022-03-03 10:51:42 -0600

    from

---
+++ server/models/PostForm.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const PostSchema = new Schema(
+    {
+      title: {
+        type: String,
+        required: true,
+        max_length: 50,
+      },
+      respitory: {
+        type: String,
+        required: true,
+        max_length: 50,
+        unique: true,
+      },
+      liveLink: {
+          type: String,
+          requried: true,
+         min_length: 2,
+         unique: true,
+        },
+     
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
+  
+  
+ 
+  
+  
+  const User = model('User', UserSchema);
+  
+  module.exports = User;
