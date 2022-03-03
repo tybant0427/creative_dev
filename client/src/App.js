87691ce843f814e057bf02cd906c729cfc2de505
Tyler Bantley <tyler.bantley@icloud.com>
2022-03-03 11:56:56 -0600

    merging

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./components/login";
-import SignUp from "./components/signup";
+import SignUp from "./components/signup"
 function App() {
   return (<Router>
     <div className="App">
@@ -27,7 +27,7 @@ function App() {
           <Switch>
             <Route exact path='/' component={Login} />
             <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
+            <Route path="/sign-up" component={SignUp} />  
           </Switch>
         </div>
       </div>
