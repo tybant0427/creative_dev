fbb1476fc95b553d2eb2b7c40c276ad3fd34d346
Tyler Bantley <tyler.bantley@icloud.com>
2022-03-05 23:03:27 -0600

    updating project cards

--- client/src/components/BlogComponents/CardProjects.js
+++ client/src/components/BlogComponents/CardProjects.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ListGroup, Card, ListGroupItem,  } from 'react-bootstrap';
+
 
 const Layout = ({ projects }) => {
   if (!projects.length) {
@@ -10,7 +12,36 @@ const Layout = ({ projects }) => {
       {/* <h3>{title}</h3> */}
       {
         projects.map((thought) => (
-          <div key={thought._id} className="card mb-3">
+          <Card style={{ width: '18rem' }} key={thought._id}>
+          {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
+          <Card.Body>
+            <Card.Title>{thought.title}</Card.Title>
+            <Card.Text>
+              {thought.description}
+            </Card.Text>
+          </Card.Body>
+          {/* <ListGroup className="list-group-flush">
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          </ListGroup> */}
+          <Card.Body>
+            <Card.Link href="#">{thought.respitoryLink}</Card.Link>
+            <Card.Link href="#">{thought.liveLink}</Card.Link>
+          </Card.Body>
+        </Card>
+        ))}
+    </div>
+  );
+};
+
+export default Layout;
+
+
+
+
+
+          {/* <div key={thought._id} className="card mb-3">
             <h4 className="card-header bg-primary text-light p-2 m-0">
               {thought.description} <br />
               <span style={{ fontSize: '1rem' }}>
@@ -20,10 +51,4 @@ const Layout = ({ projects }) => {
             <div className="card-body bg-light p-2">
               <p>{thought.thoughtText}</p>
             </div>
-          </div>
-        ))}
-    </div>
-  );
-};
-
-export default Layout;
+          </div> */}
