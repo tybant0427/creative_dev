d23533f2a97946e6a2186c0c77d4ff85b3726684
Grant Royseth <groyseth@gmail.com>
2022-03-08 11:04:41 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -25,9 +25,9 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
+ 
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
+ 
   return {
     headers: {
       ...headers,
