25ea6eebbc6158609b22c16cbba5df22e2aed177
Grant Royseth <groyseth@gmail.com>
2022-03-10 12:45:21 -0600

    commit

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
   
   db.once('open', () => {
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
